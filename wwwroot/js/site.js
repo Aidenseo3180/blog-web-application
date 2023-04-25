e13311d53cd65b5c6feea7b4fadaa54778c8f351
Aiden Seo <minwoo3180@gmail.com>
2023-04-24 20:08:36 -0400

    updated

---
+++ wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿//Fade effect of success/fail messages
+window.setTimeout(function () {
+    $(".alert").fadeTo(500, 0).slideUp(500, function () {
+        $(this).remove();
+    });
+}, 4000);
+
+function myFunction() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
